a1aa1fbed4d94868aca3374a9e869af3471a639d
Gabriel Schrock <gabriel.schrock@wolflair.com>
2019-11-22 10:28:34 -0800

    make pentagram static asset

--- src/components/Pentagram.tsx
+++ src/components/Pentagram.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import pentacle_animated from "../static/pentacle_animated.svg";
 
 interface Props {
   className?: string;
@@ -8,12 +9,7 @@ interface Props {
 interface ImplProps extends Props {}
 
 const PentagramImpl: React.SFC<ImplProps> = ({ className }) => (
-  <img
-    className={className}
-    src="https://svgshare.com/i/GGu.svg"
-    title="pentacle_animated"
-    alt="pentagram_img"
-  />
+  <div className={className} />
 );
 
 const StyledPentagram = styled(PentagramImpl)`
@@ -21,6 +17,9 @@ const StyledPentagram = styled(PentagramImpl)`
   padding-bottom: 1.25rem;
   transform: rotate(180deg);
   opacity: 0.3;
+  background-image: url(${pentacle_animated});
+  background-position: center;
+  background-repeat: no-repeat;
 `;
 
 export default StyledPentagram;
